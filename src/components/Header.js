3eabc72470cded09cf36257919fe7c884db9391e
Josh Z <joshz@Joshs-MacBook-Pro.local>
2021-07-01 15:27:08 -0400

    updates logout function

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import { useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { GlobalContext } from '../context/GlobalState';
 
 const Header = () => {
+	const { userInfo, logoutUser, checkForToken } = useContext(GlobalContext);
+
+	useEffect(() => {
+		checkForToken();
+	}, []);
+
+	const handleLogout = () => {
+		logoutUser();
+	};
+
 	return (
 		<div>
+			{userInfo && <p>{userInfo.name}</p>}
 			<Link to="/">Home</Link>
 			<Link to="/packages">Packages</Link>
 			<Link to="/users/login">Login</Link>
+			<button onClick={handleLogout}>Logout</button>
 		</div>
 	);
 };
