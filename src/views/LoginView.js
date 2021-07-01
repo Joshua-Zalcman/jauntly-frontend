b79efe191a6140bbcf08e2b17bffba5de5a41f94
Josh Z <joshz@Joshs-MacBook-Pro.local>
2021-07-01 17:42:31 -0400

    updates login view

--- src/views/LoginView.js
+++ src/views/LoginView.js
@@ -9,21 +9,25 @@ const LoginView = ({ URL, history }) => {
 	const [password, setPassword] = useState('');
 
 	const loginUser = async (email, password) => {
-		const response = await axios.post(
-			`${URL}/users/login`,
-			{ email, password },
-			{
-				headers: {
-					'Content-Type': 'Application/json',
-				},
+		try {
+			const response = await axios.post(
+				`${URL}/users/login`,
+				{ email, password },
+				{
+					headers: {
+						'Content-Type': 'Application/json',
+					},
+				}
+			);
+			if (response.data.token) {
+				localStorage.setItem('token', response.data.token);
+				checkForToken();
+				history.push('/packages');
+			} else {
+				setMessage(response.data.message);
 			}
-		);
-		if (response.data.token) {
-			localStorage.setItem('token', response.data.token);
-			checkForToken();
-			history.push('/packages');
-		} else {
-			setMessage(response.data.message);
+		} catch (err) {
+			console.log(err);
 		}
 	};
 
